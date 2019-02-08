6d77e7db052d561ac9d394ac34b69625c77864f9
mysho <mysho@smile.fr>
2019-02-08 18:18:44 +0200

    #Alanstorm m1. Magento for developers. Compleate

--- vagrant/projects/magento.on/app/code/local/Alanstormdotcom/Helloworld/controllers/IndexController.php
+++ vagrant/projects/magento.on/app/code/local/Alanstormdotcom/Helloworld/controllers/IndexController.php
@@ -47,4 +47,45 @@ class Alanstormdotcom_Helloworld_IndexController extends Mage_Core_Controller_Fr
         echo '</dl>';
     }
 
+    // https://alanstorm.com/magento_collections/
+    public function collectionsAction() {
+
+        $thing_1 = new Varien_Object();
+        $thing_1->setName('Richard');
+        $thing_1->setAge(24);
+
+        $thing_2 = new Varien_Object();
+        $thing_2->setName('Jane');
+        $thing_2->setAge(12);
+
+        $thing_3 = new Varien_Object();
+        $thing_3->setName('Spot');
+        $thing_3->setLastName('The Dog');
+        $thing_3->setAge(7);
+
+        // var_dump($thing_1->getName());
+
+        $collection_of_things = new Varien_Data_Collection();
+        $collection_of_things
+            ->addItem($thing_1)
+            ->addItem($thing_2)
+            ->addItem($thing_3);
+
+        // var_dump($collection_of_things->getFirstItem()->getData());
+
+        // var_dump( $collection_of_things->toXml() );
+
+        $collection_of_products = Mage::getModel('catalog/product')->getCollection();
+
+         // var_dump($collection_of_products->getSelect() ); // це я вибрав дані з бази. var_dump виведе обєкт Varien_Db_Select
+         // var_dump($collection_of_products->getData() ); // вивів ті дані що я вибрав
+
+
+
+        var_dump( (string) $collection_of_products->getSelect() );
+
+
+
+    }
+
 }
