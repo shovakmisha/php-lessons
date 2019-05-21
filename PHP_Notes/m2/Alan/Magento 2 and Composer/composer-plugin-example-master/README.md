23a0172a0304a4a7caeb2083d84f908d8c297390
mysho <mysho@smile.fr>
2019-05-21 17:29:05 +0300

    #AlanStorm Composer finish

---
+++ PHP_Notes/m2/Alan/Magento 2 and Composer/composer-plugin-example-master/README.md
@@ -0,0 +1,5 @@
+# composer-plugin-example
+
+Simple example of a composer plugin. 
+
+Has a tagged 0.0.1, use to demonstrate updating from 0.0.1 to dev-master.
---
+++ PHP_Notes/m2/Alan/Magento 2 and Composer/composer-plugin-example-master/src/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+namespace Pulsestorm\Composer\Example;
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\EventDispatcher\EventSubscriberInterface;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+    }
+    
+    public static function getSubscribedEvents()
+    {
+        return array(
+            'post-install-cmd' => 'installOrUpdate',
+            'post-update-cmd' => 'installOrUpdate',            
+        );
+    }    
+    
+    public function installOrUpdate($event)
+    {
+        file_put_contents('/tmp/composer.log', __METHOD__ . "\n",FILE_APPEND);
+        file_put_contents('/tmp/composer.log', get_class($event) . "\n",FILE_APPEND);            
+        file_put_contents('/tmp/composer.log', $event->getName() . "\n",FILE_APPEND);                    
+    }
+}
